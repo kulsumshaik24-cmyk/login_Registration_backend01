62ddda4735a5197c6b0942ce20706aeca632d364
kulsum shaik <kulsum.shaik24@sasi.ac.in>
2026-07-14 15:30:25 +0530

    Refactor db.js for consistent formatting

--- config/db.js
+++ config/db.js
@@ -6,9 +6,8 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const connectDB = async () => {
   try {
-    // MongoDB Atlas connection string
-    const connString = "mongodb+srv://KulsumShaik24_db_user:kulsum20@cluster0.nxftcfq.mongodb.net/kulsum?appName=cluster0";
-    
+    const connString = 'mongodb+srv://KulsumShaik24_db_user:kulsum20@cluster0.nxftcfq.mongodb.net/kulsum?appName=cluster0';
+
     await mongoose.connect(connString);
     console.log('MongoDB connected successfully!');
   } catch (err) {
