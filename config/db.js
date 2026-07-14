32beb698b13b33e45c2e504cdf5cf138f2edf281
kulsum shaik <kulsum.shaik24@sasi.ac.in>
2026-07-14 16:05:25 +0530

    Enhance MongoDB connection with timeout option
    Added a connection timeout to the MongoDB connection.

--- config/db.js
+++ config/db.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
+const dns = require('dns');
 
-const dns = require("dns");
-
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
+// Use public DNS servers as a fallback if the environment blocks SRV lookups
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 const connectDB = async () => {
   try {
     const connString = 'mongodb+srv://KulsumShaik24_db_user:kulsum20@cluster0.nxftcfq.mongodb.net/kulsum?appName=cluster0';
 
-    await mongoose.connect(connString);
+    await mongoose.connect(connString, { connectTimeoutMS: 10000 });
     console.log('MongoDB connected successfully!');
   } catch (err) {
     console.error('Database connection error:', err.message);
